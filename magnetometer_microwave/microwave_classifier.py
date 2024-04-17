1779da67bef31e742b04336f543577da918a7514
Beza Nigatu <77472940+diressa@users.noreply.github.com>
2024-04-17 14:16:33 -0400

    Updated microwave_classifier.py to use a random file
    A random file is selected from "xlsx_files". 
    Previously only a single file was called directly.

--- magnetometer_microwave/microwave_classifier.py
+++ magnetometer_microwave/microwave_classifier.py
@@ -1,35 +1,31 @@
 from openpyxl import load_workbook
-from statistics import mean
+from statistics import pvariance
 from matplotlib import pyplot as plt
+import os
+import random
 
-sample = random.choice(os.listdir('random_xlsx_files')) #random file sample
-#print(sample) #remove as comment to see which file is used
-wb = load_workbook(filename='random_xlsx_files/'+sample)
+sample = random.choice(os.listdir('xlsx_files'))
+wb = load_workbook(filename='xlsx_files/' + sample)  # loop through each file
 ws = wb.active
 
 sheet_ranges = wb['Raw Data']
-data_length = len(sheet_ranges['A'])
+print(sample)
+print(sheet_ranges['A2'].value)
+
+col_x = 0
+col_y = 0
+
+for col in ws.iter_cols(min_row=2, max_col=3, max_row=len(sheet_ranges['A']), values_only=True):
+    list(col)    # min_row=2 because cannot calculate a mean avg with string in first row
+
+for col_x in ws.iter_cols(min_col=1, min_row=2, max_col=1, max_row=len(sheet_ranges['A']), values_only=True):
+    list(col_x)  # x-axis, time in seconds
+
+for col_y in ws.iter_cols(min_col=3, min_row=2, max_col=3, max_row=len(sheet_ranges['A']), values_only=True):
+    list(col_y)  # y-axis, magnitude of y-direction microteslas
 
 
-for col_x in ws.iter_cols(min_col=1, min_row=2, max_col=1, max_row=data_length, values_only=True):
-    list(col_x)#print(col_x) # x-axis, time in seconds
-
-for col_y in ws.iter_cols(min_col=3, min_row=2, max_col=3, max_row=data_length, values_only=True):
-    list(col_y) # y-axis, magnitude of y-direction microteslas
-
-classification = ''
-#print(pvariance(col_y))
-
-if pvariance(col_y)<0.5:
-    classification = 'Nothing'
-    print(classification)  # later can group files or label?
-elif pvariance(col_y)>=0.5:
-    if sheet_ranges['C2'].value > sheet_ranges['C' + str(data_length)].value:
-        classification = 'Close to open'
-        print(classification)
-    else:
-        classification = 'Open to close'
-        print(classification)
+print(pvariance(col_y))
 
 
 plt.plot(col_x, col_y, linewidth=2, color='r')
@@ -37,4 +33,3 @@ plt.title('Y-Direction of Microteslas')
 plt.xlabel('Time in Seconds')
 plt.ylabel('Microteslas')
 plt.show()
-
